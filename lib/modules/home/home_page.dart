d842ad7622413294cc92489f3385b254b9bc0639
felipe baloneker <felipebaloneker23@gmail.com>
2022-03-30 23:28:39 -0300

    criada appbar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import '../../shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,7 +8,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomePage")),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(152),
+          child: Container(
+              height: 152,
+              color: AppColors.primary,
+              child: Center(
+                child: ListTile(
+                  title: Text.rich(TextSpan(
+                      text: "Ola, ",
+                      style: TextStyles.titleRegular,
+                      children: [
+                        TextSpan(
+                            text: "Gabul",
+                            style: TextStyles.titleBoldBackground)
+                      ])),
+                  subtitle: Text(
+                    "Mantenha suas contas em dia",
+                    style: TextStyles.captionShape,
+                  ),
+                  trailing: Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                ),
+              ))),
     );
   }
 }
