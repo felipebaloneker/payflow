bd1a76cddf6b812bdb89789bf217e1938070117f
felipe baloneker <felipebaloneker23@gmail.com>
2022-04-26 10:42:29 -0300

    updated home layout

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -36,6 +36,39 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ))),
+      bottomNavigationBar: Container(
+          height: 90,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: () {
+                print:
+                ("Clicou");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add_box_outlined,
+                      color: AppColors.background,
+                    )),
+              ),
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.description_outlined, color: AppColors.body))
+          ])),
     );
   }
 }
